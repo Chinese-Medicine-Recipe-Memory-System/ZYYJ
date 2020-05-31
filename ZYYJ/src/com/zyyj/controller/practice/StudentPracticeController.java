0521bfe3d1e23c0ec9066014f1f074defbe61fb2
xxJAY <45217931+StellarisJAY@users.noreply.github.com>
2020-05-31 20:25:21 +0800

    Update StudentPracticeController.java

--- ZYYJ/src/com/zyyj/controller/practice/StudentPracticeController.java
+++ ZYYJ/src/com/zyyj/controller/practice/StudentPracticeController.java
@@ -16,13 +16,14 @@ import com.zyyj.service.practice.StudentPracticeService;
 public class StudentPracticeController {
 	
 	@Autowired
-	private StudentPracticeService service;
+	private StudentPracticeService service;     // service实体
 	
 	@RequestMapping(value="searchStudentPractice", method=RequestMethod.POST)
 	public void searchStudentPractice(String input, String option, HttpServletResponse resp) throws IOException {
-		String result = service.getSearchResult(input, option);
+		String result = service.getSearchResult(input, option);    // 从servie获取表格形式的搜索结果
 		
-		resp.setHeader("Content-Type", "text/plain;charset=utf-8");
+		
+		resp.setHeader("Content-Type", "text/plain;charset=utf-8"); // 发送搜索结果到前端
 		PrintWriter out = resp.getWriter();
 		out.write(result);
 	}
