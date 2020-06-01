3d95a5a253d06d317c8fb3c285222ea06a60b7a5
xxJAY <45217931+StellarisJAY@users.noreply.github.com>
2020-06-01 08:32:19 +0800

    Update StudentExamMapper.java

--- ZYYJ/src/com/zyyj/mapper/exam/StudentExamMapper.java
+++ ZYYJ/src/com/zyyj/mapper/exam/StudentExamMapper.java
@@ -6,11 +6,11 @@ import com.zyyj.bean.StudentExam;
 
 public interface StudentExamMapper {
 	
-	List<StudentExam> getAllStudentExams();
+	List<StudentExam> getAllStudentExams();            // 获取全部学生
 	
-	List<StudentExam> searchByStudent(String stuName);
+	List<StudentExam> searchByStudent(String stuName); // 按照学生姓名搜索
 	
-	List<StudentExam> searchByClass(String className);
+	List<StudentExam> searchByClass(String className); // 按照班级搜索 
 	
-	List<StudentExam> searchByExam(String exam_name);
+	List<StudentExam> searchByExam(String exam_name);  // 按照考试方案搜索
 }
