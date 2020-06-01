53cfa0a7bb84d8b2a03aa69e784aa94836e6588b
xxJAY <45217931+StellarisJAY@users.noreply.github.com>
2020-06-01 08:35:33 +0800

    Update ExchangeIntegralMapper.java

--- ZYYJ/src/com/zyyj/mapper/credit/ExchangeIntegralMapper.java
+++ ZYYJ/src/com/zyyj/mapper/credit/ExchangeIntegralMapper.java
@@ -6,16 +6,16 @@ import com.zyyj.bean.ExchangeIntegral;
 
 public interface ExchangeIntegralMapper {
 	
-	List<ExchangeIntegral> getAllExchangeIntegrals();
+	List<ExchangeIntegral> getAllExchangeIntegrals();         // 获取所有积分兑换记录
 	
-	List<ExchangeIntegral> searchByClass(String className);
+	List<ExchangeIntegral> searchByClass(String className);   // 按照班级搜索
 	
-	List<ExchangeIntegral> searchByStudent(String stuName);
+	List<ExchangeIntegral> searchByStudent(String stuName);   // 按照学生姓名搜索
 	
-	List<ExchangeIntegral> searchByStudentId(String stuId);
+	List<ExchangeIntegral> searchByStudentId(String stuId);   // 按照学号搜索
 	
-	List<ExchangeIntegral> searchByMajor(String major);
+	List<ExchangeIntegral> searchByMajor(String major);       // 按照专业搜索
 	
-	List<ExchangeIntegral> searchByGrade(String grade);
+	List<ExchangeIntegral> searchByGrade(String grade);       // 按照年级搜索
 	
 }
