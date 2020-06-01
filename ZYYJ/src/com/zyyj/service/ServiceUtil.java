76354bb9697e3e5f588cc9efefddb2375be2754e
xxJAY <45217931+StellarisJAY@users.noreply.github.com>
2020-06-01 19:33:25 +0800

    更改了自动生成ID的方法

--- ZYYJ/src/com/zyyj/service/ServiceUtil.java
+++ ZYYJ/src/com/zyyj/service/ServiceUtil.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 public class ServiceUtil {
 	
-	public static String getNextId(String max_id, String prefix) {          // 为各种实体生成规范的ID
+	public static String getNextId(String max_id, String prefix) {
 		String next_id = max_id;
 		int next = Integer.valueOf(max_id.substring(prefix.length() + 1)) + 1;
 		if(next < 10) {
-			next_id = prefix + "_00" + next;
+			next_id = prefix + "00" + next;
 		}
 		else if(next < 100) {
-			next_id = prefix + "_0" + next;
+			next_id = prefix + "0" + next;
 		}
 		else {
-			next_id = prefix + "_" + next;
+			next_id = prefix + next;
 		}
 		return next_id;
 	}
